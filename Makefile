0b09444b1616ec728386b1f7230146d53cce6bd6
MC6312 <mc6312@gmail.com>
2019-12-19 15:27:34 +0300

    1.0

---
+++ Makefile
@@ -0,0 +1,45 @@
+packer = 7z
+pack = $(packer) a -mx=9
+arcx = .7z
+docs = COPYING Changelog README.md wishlist.json
+basename = pytodo
+srcversion = pytodo
+version = $(shell python3 -c 'from $(srcversion) import VERSION; print(VERSION)')
+branch = $(shell git symbolic-ref --short HEAD)
+title_version = $(shell python3 -c 'from $(srcversion) import TITLE_VERSION; print(TITLE_VERSION)')
+zipname = $(basename).zip
+arcname = $(basename)$(arcx)
+srcarcname = $(basename)-$(branch)-src$(arcx)
+srcs = pytodo.py
+backupdir = ~/shareddocs/pgm/python/
+
+app:
+	$(pack) -tzip $(zipname) $(srcs)
+	@echo '#!/usr/bin/env python3' >$(basename)
+	@cat $(zipname) >>$(basename)
+	rm $(zipname)
+	chmod 755 $(basename)
+
+archive:
+	$(pack) $(srcarcname) *.py *. Makefile *.geany $(docs)
+distrib:
+	make app
+	$(eval distname = $(basename)-$(version)$(arcx))
+	$(pack) $(distname) $(basename) $(docs)
+	mv $(distname) ~/downloads/
+backup:
+	make archive
+	mv $(srcarcname) $(backupdir)
+update:
+	$(packer) x -y $(backupdir)$(srcarcname)
+commit:
+	git commit -a -uno -m "$(version)"
+docview:
+	$(eval docname = README.htm)
+	@echo "<html><head><meta charset="utf-8"><title>$(title_version) README</title></head><body>" >$(docname)
+	markdown_py README.md >>$(docname)
+	@echo "</body></html>" >>$(docname)
+	x-www-browser $(docname)
+	#rm $(docname)
+show-branch:
+	@echo "$(branch)"
---
+++ pytodo.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" pytodo.py
+
+    Copyright 2019 MC-6312
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
+
+import tokenize
+import os, os.path
+import sys
+
+
+TITLE = 'PyToDo'
+VERSION = '1.0'
+TITLE_VERSION = f'{TITLE} v{VERSION}'
+
+CMT_PREFIXES = {None, tokenize.STRING, tokenize.COMMENT, tokenize.INDENT, tokenize.DEDENT, tokenize.NL, tokenize.NEWLINE}
+TODO_PREFIX = '@TODO'
+TODO_PREFIX_LEN = len(TODO_PREFIX)
+
+"""@TODO file name display"""
+
+
+def filter_string(token):
+    s = token.string
+
+    if token.type == tokenize.COMMENT:
+        # remove starting "#"
+        return s[1:].strip()
+    else:
+        # STRING type, remove all starting/ending quotation marks
+
+        qmark = s[0]
+        start = 1
+        end = len(s) - 1
+
+        while start < end and s[start] == qmark: start += 1
+        while end > start and s[end] == qmark: end -= 1
+
+        return s[start:end + 1].strip()
+
+
+def find_todo_strings(filename):
+    """Search @TODO prefixes in comments and docstrings into filename.
+
+    Returns two-tuple.
+    1st element: boolean (True if no errors);
+    2nd element:
+        if 1st == True: list of two-tuples with line numbers and strings;
+        if 1st == False: string with error message."""
+
+    if not os.path.exists(filename):
+        return (False, f'{filename} is not found')
+
+    if os.path.splitext(filename)[1].lower() != '.py':
+        return (False, f'{filename} is not Python script')
+
+    todos = []
+
+    with open(filename, 'rb') as f:
+        tokens = tokenize.tokenize(f.readline)
+
+        lasttype = None
+
+        for tkn in tokens:
+            if tkn.type in (tokenize.COMMENT, tokenize.STRING) and lasttype in CMT_PREFIXES:
+                s = filter_string(tkn)
+                if s.startswith(TODO_PREFIX):
+                    # cleaning
+                    s = list(map(lambda v: v.strip(), s[TODO_PREFIX_LEN:].splitlines()))
+
+                    todos.append((tkn.start[0], s))
+
+            lasttype = tkn.type
+
+    return (True, todos)
+
+
+def format_todo_strings(todos):
+    TODO = 'TODO at '
+    TODO_LEN = len(TODO)
+
+    maxlnw = 0
+    linenumbers = []
+
+    for todolnum, todostrs in todos:
+        lnumstr = str(todolnum)
+        lnumstrl = len(lnumstr)
+        if lnumstrl > maxlnw:
+            maxlnw = lnumstrl
+
+    maxlnw += 1
+
+    tabfmt = f'%{TODO_LEN}s%{maxlnw}s %s'
+
+    for todolnum, todostrs in todos:
+        print(tabfmt % (TODO, '%d:' % todolnum, todostrs[0]))
+
+        for todostr in todostrs[1:]:
+            print(tabfmt % ('', '', todostr))
+
+
+def main(args):
+    if len(args) < 2:
+        print(f'{__file__} filename.py [... filename.py]', file=sys.stderr)
+        return 1
+
+    for arg in args[1:]:
+        ok, r = find_todo_strings(arg)
+        if ok:
+            format_todo_strings(r)
+        else:
+            print(r, file=sys.stderr)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.argv.append(__file__)
+    sys.exit(main(sys.argv))
